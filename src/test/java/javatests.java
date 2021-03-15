07f01d658509e75a9ef56e4aedd9a9355bd6ea62
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-03-15 17:26:15 +0400

    Initial commit

---
+++ src/test/java/javatests.java
@@ -0,0 +1,86 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import javax.swing.*;
+
+public class javatests {
+    private WebDriver webDriver;
+
+    @Before
+    public void start(){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.get("https://savkk.github.io/selenium-practice/");
+    }
+
+    @Test
+    public void firstTask(){
+        WebElement firstTask =webDriver.findElement(By.id("button"));
+        firstTask.click();
+        WebElement firstClickMe= webDriver.findElement(By.id("first"));
+        firstClickMe.click();
+        WebElement resultFirstClicMe = webDriver.findElement(By.xpath(".//label[text()='Excellent!']"));
+        Assert.assertEquals("Excellent!",resultFirstClicMe.getText());
+        WebElement buttonClicMeTo = webDriver.findElement(By.xpath("//input[@type='button']"));
+        buttonClicMeTo.click();
+        WebElement returnFirstClicMe = webDriver.findElement(By.linkText("Great! Return to menu"));
+        Assert.assertEquals("Great! Return to menu",returnFirstClicMe.getText());
+        returnFirstClicMe.click();
+
+    }
+    @Test
+    public void SecondTask(){
+        WebElement secondTask = webDriver.findElement(By.id("checkbox"));
+        secondTask.click();
+        WebElement CheckBoxOne = webDriver.findElement(By.xpath("//*[@id='one']"));
+        WebElement CheckboxTwo = webDriver.findElement(By.xpath("//*[@id='two']"));
+        WebElement CheckboxThree = webDriver.findElement(By.xpath("//*[@id='three']"));
+       // WebElement Radio
+        CheckBoxOne.click();
+        WebElement buttonCheckBox = webDriver.findElement(By.id("go"));
+        buttonCheckBox.click();
+        WebElement resultCheckBox = webDriver.findElement(By.id("result"));
+        Assert.assertEquals("one",resultCheckBox.getText().trim());
+        CheckboxTwo.click();
+        buttonCheckBox.click();
+        Assert.assertEquals("one two",resultCheckBox.getText().trim());
+        CheckboxThree.click();
+        buttonCheckBox.click();
+        Assert.assertEquals("one two three",resultCheckBox.getText().trim());
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    @After
+    public void teardown () throws InterruptedException {
+        Thread.sleep(5000);
+        if (webDriver !=null){
+            webDriver.quit();
+        }
+    }
+
+
+}
